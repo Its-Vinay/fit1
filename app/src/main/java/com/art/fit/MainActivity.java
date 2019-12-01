d86aaa66f924474ae34a715a7dbdf2d492f89fcc
Its-vinay <vinay161998@gmail.com>
2019-12-02 01:19:04 +0530

    OnRebootReceiver.java is added

--- app/src/main/java/com/art/fit/MainActivity.java
+++ app/src/main/java/com/art/fit/MainActivity.java
@@ -1,9 +1,9 @@
 package com.art.fit;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -11,7 +11,6 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,9 +18,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
 
     TextView tv_steps;
-    Button btnadd,btnremove;
+    Button btnadd,btnremove,btn_reset,btn_train;
     TextView distance,water,calorie;
     int count=0;
+    int ref;
 
     SensorManager sensorManager;
     boolean running=false;
@@ -29,14 +29,51 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv_steps = (TextView) findViewById(R.id.tv_steps);
-        distance=(TextView) findViewById(R.id.tv_steps2);
-        water=(TextView) findViewById(R.id.textView2);
+        tv_steps =  findViewById(R.id.tv_steps);
 
-        calorie=(TextView) findViewById(R.id.calorie);
-        btnadd       = (Button)findViewById(R.id.btnadd);
-        btnremove    = (Button)findViewById(R.id.btnremove);
+        btnadd       = findViewById(R.id.btn_add);
+        btnremove    = findViewById(R.id.btn_remove);
+        btn_reset =  findViewById(R.id.btn_reset);
+        btn_train = findViewById(R.id.btn_train);
+        distance= findViewById(R.id.tv_distance);
+        water= findViewById(R.id.tv_water);
+
+        calorie= findViewById(R.id.tv_calories);
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        SharedPreferences preferences = getSharedPreferences("STEP_COUNTER_REF",MODE_PRIVATE);
+        ref = preferences.getInt("STEP_REF",0);
+
+
+        btnadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                add(v);
+            }
+        });
+
+        btnremove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                remove(v);
+            }
+        });
+
+        btn_train.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Intent intent;
+                intent = new Intent(getApplicationContext(),excercise.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                reset();
+            }
+        });
 
 
 
@@ -69,17 +106,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
 
 
-    public void intentEvent(View view) {
-
-        Intent intent;
-        switch (view.getId()) {
-
-            case R.id.button:
-                intent = new Intent(getApplicationContext(),excercise.class);
-                startActivity(intent);
-
-        }
-    }
 
     @Override
     public void onSensorChanged(SensorEvent Event) {
@@ -87,11 +113,31 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         if(running)
         {
-            tv_steps.setText(String.valueOf(Event.values[0]));
-            distance.setText(String.valueOf((Event.values[0]*0.000762)));
-            calorie.setText(String.valueOf((Event.values[0]*.04)));
+            float c = Event.values[0];
+            c  -= ref;
+            tv_steps.setText(String.valueOf((int)c));
+            distance.setText(String.valueOf((c*0.76)));
+            calorie.setText(String.valueOf((c*.05)));
+
 
         }
+
+    }
+
+
+        private void reset()
+        {
+            ref = count;
+            count  = 0;
+            SharedPreferences.Editor editor = getSharedPreferences("STEP_COUNT_REF",MODE_PRIVATE).edit();
+            editor.putInt("STEP_REF",ref);
+
+
+            tv_steps.setText("0");
+            distance.setText("0");
+            calorie.setText("0");
+            water.setText("0");
+
 
     }
 
@@ -103,13 +149,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void add(View view) {
 
         count+=1;
-        water.setText(count+"");
+        water.setText(String.valueOf(count));
     }
 
     public void remove(View view) {
 
         count-=1;
-        water.setText(count+"");
+        water.setText(String.valueOf(count));
     }
 }
 
